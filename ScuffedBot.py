f23dc989e97d6a2ee3e2001a03552edaadc2d91f
thijnmens <57702588+thijnmens@users.noreply.github.com>
2020-12-23 14:59:35 +0100

    Update ScuffedBot.py

--- ScuffedBot.py
+++ ScuffedBot.py
@@ -19,7 +19,7 @@ cred = credentials.Certificate({
   "type": os.getenv("TYPE"),
   "project_id": os.getenv("PROJECT_ID"),
   "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-  "private_key": getenv("PRIVATE_KEY"),
+  "private_key": os.getenv("PRIVATE_KEY"),
   "client_email": os.getenv("CLIENT_EMAIL"),
   "client_id": os.getenv("CLIENT_ID"),
   "auth_uri": os.getenv("AUTH_URI"),
