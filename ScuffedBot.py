01f6b2029d89f156054ea6ecee31c58ce1db975d
thijnmens <57702588+thijnmens@users.noreply.github.com>
2020-12-23 14:20:31 +0100

    Update ScuffedBot.py

--- ScuffedBot.py
+++ ScuffedBot.py
@@ -15,7 +15,18 @@ from firebase_admin import firestore
 from firebase_admin import db
 
 client = discord.Client()
-cred = credentials.Certificate("ServiceAccountKey.json")
+cred = credentials.Certificate({
+  "type": os.getenv(TYPE),
+  "project_id": os.getenv(PROJECT_ID),
+  "private_key_id": os.getenv(PRIVATE_KEY_ID),
+  "private_key": os.getenv(PRIVATE_KEY),
+  "client_email": os.getenv(CLIENT_EMAIL),
+  "client_id": os.getenv(CLIENT_ID),
+  "auth_uri": os.getenv(AUTH_URI),
+  "token_uri": os.getenv(TOKEN_URI),
+  "auth_provider_x509_cert_url": os.getenv(AUTH_PROVIDER_X509_CERT_URL),
+  "client_x509_cert_url": os.getenv(CLIENT_X509_CERT_URL)
+})
 default_app = firebase_admin.initialize_app(cred)
 dab = firestore.client()
 
