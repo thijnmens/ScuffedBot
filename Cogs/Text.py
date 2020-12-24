7c487ceff47eaf4011ebc69da7916b4095514404
thijnmens <57702588+thijnmens@users.noreply.github.com>
2020-12-24 13:39:12 +0100

    lets hope i didnt break shit

--- Cogs/Text.py
+++ Cogs/Text.py
@@ -1,6 +1,24 @@
-import discord, os, json, requests
+import discord, os, json, requests, schedule, time, firebase_admin, asyncio
 from discord.ext import commands
 from discord.utils import get
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import db
+
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": os.getenv("PROJECT_ID").replace('\\n', '\n'),
+  "private_key_id": os.getenv("PRIVATE_KEY_ID").replace('\\n', '\n'),
+  "private_key": os.getenv("PRIVATE_KEY").replace('\\n', '\n'),
+  "client_email": os.getenv("CLIENT_EMAIL").replace('\\n', '\n'),
+  "client_id": os.getenv("CLIENT_ID").replace('\\n', '\n'),
+  "auth_uri": os.getenv("AUTH_URI").replace('\\n', '\n'),
+  "token_uri": os.getenv("TOKEN_URI").replace('\\n', '\n'),
+  "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL").replace('\\n', '\n'),
+  "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL").replace('\\n', '\n')
+})
+default_app = firebase_admin.initialize_app(cred)
+dab = firestore.client()
 
 #Get Random Quote
 def get_quote():
@@ -8,6 +26,20 @@ def get_quote():
     json_data = json.loads(responce.text)
     quote = json_data[0]['q'] + ' -' + json_data[0]['a']
     return(quote)
+
+#Check for birthdays
+def get_birthdays():
+    collectionlist = dab.collection_group()
+    print(collectionlist)
+    ref = dab.collection(collectionlist).document('data').get()
+    username = ref.get('username')
+    scoresaber = ref.get('scoresaber')
+    birthday = ref.get('birthday')
+schedule.every().day.at('12:00').do(get_birthdays)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
 
 class Text(commands.Cog):
     def __init__(self, client):
@@ -27,9 +59,7 @@ class Text(commands.Cog):
     @commands.command()
     async def test(self, ctx):
         print('Recieved: >test')
-        print(ctx.message.author)
-        print(ctx.message.content)
-        print(ctx.message.author.id)
+        get_birthdays()
         await ctx.send('testing complete')
         print('Response: testing complete')
         print('----------')
@@ -59,8 +89,8 @@ class Text(commands.Cog):
         embed.set_author(name="Thijnmens", url="https://github.com/thijnmens/", icon_url="https://cdn.discordapp.com/avatars/490534335884165121/eaeff60636ebf53040d8d5c0761c6c67.png?size=256")
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/790189114711605260/c6e486bab141b997eeceb42ac5c9a3c2.png?size=256")
         embed.add_field(name=">help", value="this fancy page", inline=False)
-        embed.add_field(name=">user <mention>", value="get the info of a user", inline=False)
-        embed.add_field(name=">user add <mention> <username> <scoresaber> <b-day>", value="add yourself to the userbase, if you don't want to fill something in, pls use ``None``", inline=False)
+        embed.add_field(name=">user [mention]", value="get the info of a user", inline=False)
+        embed.add_field(name=">user add", value="add yourself to the userbase, if you don't want to fill something in, pls use ``None``", inline=False)
         embed.add_field(name=">user update <mention> <field> <new value>", value="Update your info", inline=False)
         embed.add_field(name=">user remove <mention>", value="Removes your info from the database", inline=False)
         embed.add_field(name=">quote", value="idk, a random quote?", inline=False)
