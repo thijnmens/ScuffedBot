c32a6d24c0dd82f9d9a0732e8b0ae676273cef1c
thijnmens <57702588+thijnmens@users.noreply.github.com>
2020-12-25 17:43:23 +0100

    My sanity is gone

--- Cogs/BirthdayCheck.py
+++ Cogs/BirthdayCheck.py
@@ -15,6 +15,7 @@ check = False
 class BirthdayCheck(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.infinite_loop.start()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -38,27 +39,26 @@ class BirthdayCheck(commands.Cog):
         print('----------')
 
     #Infinite Loop
+    @tasks.loop(minutes=1)
     async def infinite_loop(self, ctx):
         await client.wait_until_ready()
         while not client.is_closed:
             now = datetime.now()
             current_time = now.strftime("%Y-%m-%d-%H-%M-%S")
             try:
-                if current_time == '2020-12-25-17-40-00':
+                if current_time == '2020-12-25-17-45 -00':
                     channel = client.get_channel(754627439413690469)
                     await channel.send('Countdown has initialized <a:Loading:792062970599178251>')
                     await channel.send('3 More Days till a̶̧͔̱̰̩̋͑̅̾͗̈́̐͂̚͘g̸̺̣̟̜̓̓́́͘h̸͖͈̺̿̊͆͒̅̎̑̚ͅa̴̙̫̗̟͐͂̈̀̒̅͛̉͠s̴̺̔̌͑͑s̷̞̥͈͚̺͈͕̀̀͂̇́͘ͅȁ̵̬̀̂̂̎͝g̸͓̞̑̐̏̉́͆͝h̷̹̯̣͈̻̺͑̾́́̔͗̐̓͘k̸̯̟̼̮̜̏͐͜....')
-                if current_time == '2020-12-26-17-40-00':
+                if current_time == '2020-12-26-17-45-00':
                     channel = client.get_channel(754627439413690469)
                     await channel.send('2 More Days till a̶̧͔̱̰̩̋͑̅̾͗̈́̐͂̚͘g̸̺̣̟̜̓̓́́͘h̸͖͈̺̿̊͆͒̅̎̑̚ͅa̴̙̫̗̟͐͂̈̀̒̅͛̉͠s̴̺̔̌͑͑s̷̞̥͈͚̺͈͕̀̀͂̇́͘ͅȁ̵̬̀̂̂̎͝g̸͓̞̑̐̏̉́͆͝h̷̹̯̣͈̻̺͑̾́́̔͗̐̓͘k̸̯̟̼̮̜̏͐͜....')
-                if current_time == '2020-12-27-17-40-00':
+                if current_time == '2020-12-27-17-45-00':
                     channel = client.get_channel(754627439413690469)
                     await channel.send('1 More Day till a̶̧͔̱̰̩̋͑̅̾͗̈́̐͂̚͘g̸̺̣̟̜̓̓́́͘h̸͖͈̺̿̊͆͒̅̎̑̚ͅa̴̙̫̗̟͐͂̈̀̒̅͛̉͠s̴̺̔̌͑͑s̷̞̥͈͚̺͈͕̀̀͂̇́͘ͅȁ̵̬̀̂̂̎͝g̸͓̞̑̐̏̉́͆͝h̷̹̯̣͈̻̺͑̾́́̔͗̐̓͘k̸̯̟̼̮̜̏͐͜....')
             except Exception as e:
                 print(e)
             await asyncio.sleep(60)
-    
-    client.loop.create_task(infinite_loop())
 
 def setup(client):
     client.add_cog(BirthdayCheck(client))
