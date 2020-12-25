2e52a68960a9ec430587880d6386d033f8d48637
thijnmens <57702588+thijnmens@users.noreply.github.com>
2020-12-25 15:18:40 +0100

    Add files via upload

--- Cogs/BirthdayCheck.py
+++ Cogs/BirthdayCheck.py
@@ -1,10 +1,12 @@
-import discord, os, requests, json, firebase_admin, asyncio, schedule, time
+import discord, os, requests, json, firebase_admin, asyncio, datetime
 from discord.ext import commands
 from discord.utils import get
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import db
 
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
 dab = firestore.client()
 
 class BirthdayCheck(commands.Cog):
@@ -18,22 +20,15 @@ class BirthdayCheck(commands.Cog):
             amount = len(ref) - 1
         except Exception as e:
             print(e)
-    
-    schedule.every().day.at("12:00").do(get_birthdays)
 
     #Test
     @commands.command()
     async def test(self, ctx):
         print('Recieved: >test')
-        get_birthdays()
+        print(current_time)
         await ctx.send('testing complete')
         print('Response: testing complete')
         print('----------')
-
-
-while True:
-    asyncio.sleep(43200)
-    schedule.run_pending()
 
 def setup(client):
     client.add_cog(BirthdayCheck(client))
