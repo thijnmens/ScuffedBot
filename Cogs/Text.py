389a5a171bb87af61189993f5675927b9f6b5ea6
Sirspam <Charlespattison555@Gmail.com>
2020-12-26 20:49:27 +0000

    Updated help

--- Cogs/Text.py
+++ Cogs/Text.py
@@ -44,7 +44,7 @@ class Text(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         print('Recieved: >help ')
-        embed=discord.Embed(title="Help", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", description="You can find all kinds of commands here, most of them are probably broken", color=0xff0000)
+        embed=discord.Embed(title="Help", url="https://www.youtube.com/watch?v=7LnQRFh_knk", description="You can find all kinds of commands here, most of them are probably broken", color=0xff0000)
         embed.set_author(name="Thijnmens", url="https://github.com/thijnmens/", icon_url="https://cdn.discordapp.com/avatars/490534335884165121/eaeff60636ebf53040d8d5c0761c6c67.png?size=256")
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/790189114711605260/c6e486bab141b997eeceb42ac5c9a3c2.png?size=256")
         embed.add_field(name=">help", value="this fancy page", inline=False)
@@ -53,11 +53,13 @@ class Text(commands.Cog):
         embed.add_field(name=">user update <field> <new value>", value="Update your info", inline=False)
         embed.add_field(name=">user remove", value="Removes your info from the database", inline=False)
         embed.add_field(name=">scoresaber [mention]", value="gets a user's ScoreSaber data,",inline=False)
+        embed.add_field(name=">scoresaber topsong [mention]", value="gets a user's top song from ScoreSaber,",inline=False)
+        embed.add_field(name=">scoresaber recentsong [mention]", value="gets a user's most recent song from ScoreSaber,",inline=False)
         embed.add_field(name=">quote", value="Posts a random quote", inline=False)
         embed.add_field(name=">ping", value="Pings Scuffed Bot", inline=False)
         embed.set_footer(text="this code was ruined by ThiJNmEnS#6059")
         await ctx.send(embed=embed)
-        print('Response: embed')
+        print('Response: help embed')
         print('----------')
 
 def setup(client):    
