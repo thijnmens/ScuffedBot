9401dcaf6301944a9ff8dce02a5d3ddccb894e76
Sirspam <Charlespattison555@Gmail.com>
2021-01-04 00:39:59 +0000

    aliases neato

--- Cogs/ScoreSaber.py
+++ Cogs/ScoreSaber.py
@@ -27,7 +27,7 @@ def songEmbed(ctx, argument, SS_id, scoresaber): #Makes the embed message for to
     print (URL1)
     response = requests.get(URL)
     json_data = json.loads(response.text)
-    recentSongs = json_data["scores"]
+    songsList = json_data["scores"]
     response = requests.get(URL1)
     json_data = json.loads(response.text)
     print ("1")
@@ -39,29 +39,32 @@ def songEmbed(ctx, argument, SS_id, scoresaber): #Makes the embed message for to
     print ("3")
     playerName = playerInfo["playerName"]
     print ("4")
-    recentSong = recentSongs[0]
+    Song = songsList[0]
     print ("5")
-    songName = recentSong["songName"]
+    songName = Song["songName"]
     print ("6")
-    songSubName = recentSong["songSubName"]
+    songSubName = Song["songSubName"]
     print ("7")
-    songAuthorName = recentSong["songAuthorName"]
+    songAuthorName = Song["songAuthorName"]
     print ("8")
-    levelAuthorName = recentSong["levelAuthorName"]
+    levelAuthorName = Song["levelAuthorName"]
     print ("9")
-    songAcc = round((int(recentSong["score"])/int(recentSong["maxScore"]))*100, 2)
+    if Song["maxScore"] == 0:
+        songAcc = "ScoreSaber API being fucky wucky"
+    else:
+        songAcc = round((int(Song["score"])/int(Song["maxScore"]))*100, 2)
     print ("10")
-    rank = recentSong["rank"]
+    rank = Song["rank"]
     print ("11")
-    if recentSong["difficulty"] == 9:
+    if Song["difficulty"] == 9:
         difficulty = "Expert+ 🟣"
-    elif recentSong["difficulty"] == 7:
+    elif Song["difficulty"] == 7:
         difficulty = "Expert 🔴"
-    elif recentSong["difficulty"] == 5:
+    elif Song["difficulty"] == 5:
         difficulty = "Hard 🟡"
-    elif recentSong["difficulty"] == 3:
+    elif Song["difficulty"] == 3:
         difficulty = "Normal 🔵"
-    elif recentSong["difficulty"] == 1:
+    elif Song["difficulty"] == 1:
         difficulty = "Easy 🟢"
     else:
         difficulty = "Please DM Sirspam thanks uwu"
@@ -78,12 +81,12 @@ def songEmbed(ctx, argument, SS_id, scoresaber): #Makes the embed message for to
     message.set_author(name=playerName, url=scoresaber, icon_url="https://new.scoresaber.com"+playerInfo["avatar"])
     message.add_field(name="Rank <a:PeepoBoing1:792487937056571392><a:PeepoBoing2:792487937257766912><a:PeepoBoing3:792487937044512768>", value=f"#{rank}", inline=False)
     message.add_field(name="Acc <:WideAcc1:792487936691535893><:WideAcc2:792487936640811028><:WideAcc3:792487936314572811><:WideAcc4:792487936636616826>", value=f"{songAcc}%", inline=False)
-    message.add_field(name="Score <:AquaCollapsed1:792487936658243614><:AquaCollapsed2:792487936272367648><:AquaCollapsed3:792487936829816863>", value=recentSong["score"], inline=False)
-    if recentSong["pp"] == 0:
+    message.add_field(name="Score <:AquaCollapsed1:792487936658243614><:AquaCollapsed2:792487936272367648><:AquaCollapsed3:792487936829816863>", value=Song["score"], inline=False)
+    if Song["pp"] == 0:
         message.add_field(name="PP <a:BurgerChamp1:792487936703725600><a:BurgerChamp2:792487936280756246><a:BurgerChamp3:792487936679215134><a:BurgerChamp4:792487936771489832>", value="Unranked", inline=False)
     else:
-        message.add_field(name="PP <a:BurgerChamp1:792487936703725600><a:BurgerChamp2:792487936280756246><a:BurgerChamp3:792487936679215134><a:BurgerChamp4:792487936771489832>", value=recentSong["pp"], inline=False)
-    message.set_image(url="https://new.scoresaber.com/api/static/covers/"+recentSong["songHash"]+".png")
+        message.add_field(name="PP <a:BurgerChamp1:792487936703725600><a:BurgerChamp2:792487936280756246><a:BurgerChamp3:792487936679215134><a:BurgerChamp4:792487936771489832>", value=Song["pp"], inline=False)
+    message.set_image(url="https://new.scoresaber.com/api/static/covers/"+Song["songHash"]+".png")
     return message
 
 class ScoreSaber(commands.Cog):
@@ -102,39 +105,40 @@ class ScoreSaber(commands.Cog):
             ID = ID[:-1]
             ctx.author = self.client.get_user(int(ID))
             print (f"Argument given, now {ctx.author.name}")
-        ref = dab.collection(str(ctx.author.id)).document('data').get()
-        scoresaber = ref.get('scoresaber')
-        SS_id = scoresaber[25:]
-        URL = (f"https://new.scoresaber.com/api/player/{SS_id}/full")
-        print (URL)
-        response = requests.get(URL)
-        json_data = json.loads(response.text)
-        if "error" in json_data:
-            return await ctx.send("Uh Oh, the codie wodie did an oopsie! uwu\nCheck if your ScoreSaber link is valid <:AYAYASmile:789578607688417310>")
-        playerInfo = json_data["playerInfo"]
-        scoreStats = json_data["scoreStats"]
-        playerCountry = playerInfo["country"]
-        playerName = playerInfo["playerName"]
-        playerCountryFlag = (f":flag_{playerCountry.lower()}:")
-        rankedAcc = round(scoreStats["averageRankedAccuracy"], 2)
-        embed=discord.Embed(
-            title = f"{playerName}'s ScoreSaber Stats <:WidePeepoHappy1:757948845362511992><:WidePeepoHappy2:757948845404585984><:WidePeepoHappy3:757948845400522812><:WidePeepoHappy4:757948845463306310>",
-            description = f"[ScoreSaber Link]({scoresaber})",
-            colour = 0xffdc1b,
-            timestamp = ctx.message.created_at
-        )
-        embed.add_field(name="Global Rank 🌐", value=playerInfo["rank"], inline=True)
-        embed.add_field(name=f"Country Rank {playerCountryFlag}", value=playerInfo["countryRank"], inline=True)
-        embed.add_field(name="PP <a:PogLick:792002791828357131>", value=playerInfo["pp"], inline=True)
-        embed.add_field(name="Ranked Acc <:PeepoAcc:792385194351001610>", value=f"{rankedAcc}%", inline=True)
-        embed.add_field(name="Total Play Count <a:ppJedi:754632378206388315>", value=scoreStats["totalPlayCount"], inline=True)
-        embed.add_field(name="Ranked Play Count 🧑‍🌾", value=scoreStats["rankedPlayCount"], inline=True)
-        embed.set_thumbnail(url="https://new.scoresaber.com"+playerInfo["avatar"])
-        await ctx.send(embed=embed)
+        async with ctx.channel.typing():
+            ref = dab.collection(str(ctx.author.id)).document('data').get()
+            scoresaber = ref.get('scoresaber')
+            SS_id = scoresaber[25:]
+            URL = (f"https://new.scoresaber.com/api/player/{SS_id}/full")
+            print (URL)
+            response = requests.get(URL)
+            json_data = json.loads(response.text)
+            if "error" in json_data:
+                return await ctx.send("Uh Oh, the codie wodie did an oopsie! uwu\nCheck if your ScoreSaber link is valid <:AYAYASmile:789578607688417310>")
+            playerInfo = json_data["playerInfo"]
+            scoreStats = json_data["scoreStats"]
+            playerCountry = playerInfo["country"]
+            playerName = playerInfo["playerName"]
+            playerCountryFlag = (f":flag_{playerCountry.lower()}:")
+            rankedAcc = round(scoreStats["averageRankedAccuracy"], 2)
+            embed=discord.Embed(
+                title = f"{playerName}'s ScoreSaber Stats <:WidePeepoHappy1:757948845362511992><:WidePeepoHappy2:757948845404585984><:WidePeepoHappy3:757948845400522812><:WidePeepoHappy4:757948845463306310>",
+                description = f"[ScoreSaber Link]({scoresaber})",
+                colour = 0xffdc1b,
+                timestamp = ctx.message.created_at
+            )
+            embed.add_field(name="Global Rank 🌐", value=playerInfo["rank"], inline=True)
+            embed.add_field(name=f"Country Rank {playerCountryFlag}", value=playerInfo["countryRank"], inline=True)
+            embed.add_field(name="PP <a:PogLick:792002791828357131>", value=playerInfo["pp"], inline=True)
+            embed.add_field(name="Ranked Acc <:PeepoAcc:792385194351001610>", value=f"{rankedAcc}%", inline=True)
+            embed.add_field(name="Total Play Count <a:ppJedi:754632378206388315>", value=scoreStats["totalPlayCount"], inline=True)
+            embed.add_field(name="Ranked Play Count 🧑‍🌾", value=scoreStats["rankedPlayCount"], inline=True)
+            embed.set_thumbnail(url="https://new.scoresaber.com"+playerInfo["avatar"])
+            await ctx.send(embed=embed)
         print ("Response: ScoreSaber UserData embed")
         print('----------')
-
-    @scoresaber.command()
+        
+    @scoresaber.command(aliases=("recentsong", "rs"))
     async def recentsong(self, ctx, argument1=None):
         print (f"Recieved >scoresaber recentsong {ctx.author.name}")
         if argument1 is not None:
@@ -142,15 +146,16 @@ class ScoreSaber(commands.Cog):
             ID = ID[:-1]
             ctx.author = self.client.get_user(int(ID))
             print (f"Argument given, now {ctx.author.name}")
-        ref = dab.collection(str(ctx.author.id)).document('data').get()
-        scoresaber = ref.get('scoresaber')
-        SS_id = scoresaber[25:]
-        argument = "recentSong"
-        await ctx.send(embed=songEmbed(ctx, argument, SS_id, scoresaber))
+        async with ctx.channel.typing():
+            ref = dab.collection(str(ctx.author.id)).document('data').get()
+            scoresaber = ref.get('scoresaber')
+            SS_id = scoresaber[25:]
+            argument = "recentSong"
+            await ctx.send(embed=songEmbed(ctx, argument, SS_id, scoresaber))
         print ("Response: ScoreSaber RecentSong embed")
         print('----------')
     
-    @scoresaber.command()
+    @scoresaber.command(aliases=("topsong", "ts"))
     async def topsong(self, ctx, argument1=None):
         print (f"Recieved >scoresaber topsong {ctx.author.name}")
         if argument1 is not None:
@@ -158,11 +163,12 @@ class ScoreSaber(commands.Cog):
             ID = ID[:-1]
             ctx.author = self.client.get_user(int(ID))
             print (f"Argument given, now {ctx.author.name}")
-        ref = dab.collection(str(ctx.author.id)).document('data').get()
-        scoresaber = ref.get('scoresaber')
-        SS_id = scoresaber[25:]
-        argument = "topSong"
-        await ctx.send(embed=songEmbed(ctx, argument, SS_id, scoresaber))
+        async with ctx.channel.typing():
+            ref = dab.collection(str(ctx.author.id)).document('data').get()
+            scoresaber = ref.get('scoresaber')
+            SS_id = scoresaber[25:]
+            argument = "topSong"
+            await ctx.send(embed=songEmbed(ctx, argument, SS_id, scoresaber))
         print ("Response: ScoreSaber TopSong embed")
         print('----------')
 
