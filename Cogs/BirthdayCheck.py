df4e1aef2517a1c968bdb163d38f04d304cfe6a4
thijnmens <57702588+thijnmens@users.noreply.github.com>
2021-01-06 12:42:45 +0100

    now i don't have to wait 12 hours

--- Cogs/BirthdayCheck.py
+++ Cogs/BirthdayCheck.py
@@ -25,46 +25,43 @@ class BirthdayCheck(commands.Cog):
     async def test(self, ctx):
         print('Recieved: >test')
         ###VVV testing here VVV###
-        try:
-            get_birthdays()
-        except Exception as e:
-            print(e)
         ###^^^ testing here ^^^###
         await ctx.send('testing complete')
         print('Response: testing complete')
         print('----------')
 
-@tasks.loop(hours=12)
-async def status(self, ctx):
-    try:
-        ref = dab.collection('collectionlist').document('data').get().get('collectionarray')
-        amount = len(ref)
-        count = 0
-        while(count < amount):
-            ID = ref[count]
-            print(ID)
-            birthday = dab.collection(str(ID)).document('data').get().get('birthday')
-            print(birthday)
-            birthdaysplit = birthday.split('/')
-            print(birthdaysplit)
-            try:
-                birthdayfinal = birthdaysplit[0] + '-' + birthdaysplit[1]
-            except Exception:
-                birthdayfinal = '32/13'
-            print(birthdayfinal)
-            current_time = now.strftime("%d-%m")
-            print(current_time)
-            a = dab.collection(str(ID)).document('data').get().get('a')
-            print(a)
-            if(birthdayfinal == current_time):
-               if(a == False):
-                    channel = self.client.get_channel(793049781554642954)
-                    await channel.send(f'<:HyperTada:796323264888307731> Happy birtday <!@{ID}>! <:HyperTada:796323264888307731>')
-                    print(f'Wished {ID} a happy birthday')
-                    a = dab.collection(str(ID)).document('data').update({'a':True})
-            count = count + 1
-    except Exception as e:
-        print(e)
+    @BirthdayCheck.commands()
+    #@tasks.loop(hours=12)
+    async def status(self, ctx):
+        try:
+            ref = dab.collection('collectionlist').document('data').get().get('collectionarray')
+            amount = len(ref)
+            count = 0
+            while(count < amount):
+                ID = ref[count]
+                print(ID)
+                birthday = dab.collection(str(ID)).document('data').get().get('birthday')
+                print(birthday)
+                birthdaysplit = birthday.split('/')
+                print(birthdaysplit)
+                try:
+                    birthdayfinal = birthdaysplit[0] + '-' + birthdaysplit[1]
+                except Exception:
+                    birthdayfinal = '32/13'
+                print(birthdayfinal)
+                current_time = now.strftime("%d-%m")
+                print(current_time)
+                a = dab.collection(str(ID)).document('data').get().get('a')
+                print(a)
+                if(birthdayfinal == current_time):
+                   if(a == False):
+                        channel = self.client.get_channel(793049781554642954)
+                        await channel.send(f'<:HyperTada:796323264888307731> Happy birtday <!@{ID}>! <:HyperTada:796323264888307731>')
+                        print(f'Wished {ID} a happy birthday')
+                        a = dab.collection(str(ID)).document('data').update({'a':True})
+                count = count + 1
+        except Exception as e:
+            print(e)
 
 def setup(client):
     client.add_cog(BirthdayCheck(client))
