7858b87a18684e4c1e9a6df006dbf0777ec88859
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-01-26 01:18:22 +0000

    Jesus christ I really need to go sleep

--- cogs/chain_enforcement.py
+++ cogs/chain_enforcement.py
@@ -13,16 +13,16 @@ class chain_enforcement(commands.Cog):
         self.client = client
 
     @commands.Cog.listener('on_message')
-    async def on_message(self, ctx):
-        if ctx.author == self.client.user:
+    async def on_message(self, message):
+        if message.author == self.client.user:
             return
-        if ctx.channel.id != chain_channel:
+        if message.channel.id != chain_channel:
             return
         logging.info("chain_enforcement triggered")
         ref = dab.collection(str("chain_data")).document("chain_data").get()
         current_chain_message = ref.get("message")
-        if ctx.content != current_chain_message:
-            await ctx.delete_message(ctx)
+        if message.content != current_chain_message:
+            await self.client.delete_message(message)
 
 def setup(client):
     client.add_cog(chain_enforcement(client))
