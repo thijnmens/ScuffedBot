1717f234de54b073e33ac75ea60318112ac36e72
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-01-26 10:26:38 +0000

    Update chain_enforcement.py

--- cogs/chain_enforcement.py
+++ cogs/chain_enforcement.py
@@ -7,24 +7,24 @@ from discord.utils import get
 from firebase_admin import credentials, firestore, db
 
 dab = firestore.client()
-chain_channel = (803259546390888458)
+chain_channel = (796012513917272085)
 muted = set()
 
 
-async def mute(ctx, current_chain_lenght):
+async def mute(message, current_chain_lenght):
     print ("muted started")
-    mute_role = await commands.RoleConverter().convert(ctx, 783042590739529789)
+    mute_role = await commands.RoleConverter().convert(message, 783042590739529789)
     time = int(current_chain_lenght * 10)
-    muted.add(ctx.author.id)
+    muted.add(message.author.id)
     print ("1")
-    await ctx.add_roles(mute_role)
-    logging.info(f"{ctx.author.id} placed in muted\n{muted}")
+    await message.add_roles(mute_role)
+    logging.info(f"{message.author.id} placed in muted\n{muted}")
     print ("2")
     await asyncio.sleep(time)
-    muted.remove(ctx.author.id)
+    muted.remove(message.author.id)
     print ("3")
-    await ctx.remove_roles(mute_role)
-    logging.info(f"{ctx.author.id} removed from muted\n{muted}")
+    await message.remove_roles(mute_role)
+    logging.info(f"{message.author.id} removed from muted\n{muted}")
 
 
 class chain_enforcement(commands.Cog):
@@ -32,25 +32,25 @@ class chain_enforcement(commands.Cog):
         self.client = client
 
     @commands.Cog.listener('on_message')
-    async def on_message(self, ctx):
-        if ctx.author == self.client.user:
+    async def on_message(self, message):
+        if message.author == self.client.user:
             return
-        if ctx.channel.id != chain_channel:
+        if message.channel.id != chain_channel:
             return
         logging.info("chain_enforcement triggered")
         current_chain_message = dab.collection(str("chain_data")).document("chain_data").get().get("message")
         current_chain_lenght = dab.collection(str("chain_data")).document("chain_data").get().get("lenght")
         print ("1")
-        if ctx.message.content != current_chain_message:
+        if message.content != current_chain_message:
             if current_chain_lenght != 0:
                 print ("2")
                 channel = self.client.get_channel(chain_channel)
                 print ("3")
-                await channel.send(f'The chain had {current_chain_lenght} messages. {ctx.author.name} has been muted!\nThe new chain message is: {ctx.message.content}')
+                await channel.send(f'The chain had {current_chain_lenght} messages. {message.author.name} has been muted!\nThe new chain message is: {message.content}')
                 print ("4")
-                await mute(ctx, current_chain_lenght)
+                await mute(message, current_chain_lenght)
                 print ("5")
-                dab.collection(str("chain_data")).document("chain_data").update({'message': ctx.message.content})
+                dab.collection(str("chain_data")).document("chain_data").update({'message': message.content})
                 dab.collection(str("chain_data")).document('chain_data').update({'lenght': 0})
                 print ("6")
         else:
