0dfdb0a91424745f5d2e2118f636fec314830292
thijnmens <thijnsmulders04@gmail.com>
2021-02-04 21:32:53 +0100

    your mom

--- cogs/shop.py
+++ cogs/shop.py
@@ -12,20 +12,23 @@ class shop(commands.Cog):
 
     @commands.group(invoke_without_command=True, case_insensitive=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def shop(self, ctx, argument=None):
+    async def shop(self, ctx, argument=None, argument2=None):
         logging.info('Recieved: >shop') 
         if(argument==None):
             embed = discord.Embed(
                 title="Shop",
                 description="Come and see, come and see! We have more then enough scams for you!",
                 color=0xff0000)
-            embed.add_field(name="<:PixelGun:806977756277375027> Gun", value='1000 Scuffed Coins', inline=False)
+            embed.add_field(name="<:PixelGun:806984027520761856> Gun", value='1000 Scuffed Coins\n ID: gun', inline=False)
             embed.set_footer(text="this code was ruined by ThiJNmEnS and carried by Sirspam")
             await ctx.send(embed=embed)
             logging.info('Response: shop embed\n----------')
+        elif(argument.lower()=='buy'):
+            if(argument2.lower()==gun):
+                ctx.send('Your mom')
         elif(argument.lower()=='gun'):
             embed = discord.Embed(
-                title="<:PixelGun:806977756277375027> Gun",
+                title="<:PixelGun:806984027520761856> Gun",
                 description="Are we in texas or something?",
                 color=0xff0000)
             embed.add_field(name="Buyable", value='Yes', inline=False)
@@ -36,8 +39,7 @@ class shop(commands.Cog):
             await ctx.send(embed=embed)
             logging.info('Response: shop embed\n----------')
 
-    
-    
+
 
 def setup(client):
     client.add_cog(shop(client))
