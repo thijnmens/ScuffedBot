17a36fe6aa67629a21ffea8b6f3b55f419fee4e1
thijnmens <thijnsmulders04@gmail.com>
2021-02-04 21:23:07 +0100

    Gun: consumable == yes

--- cogs/shop.py
+++ cogs/shop.py
@@ -12,16 +12,32 @@ class shop(commands.Cog):
 
     @commands.group(invoke_without_command=True, case_insensitive=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def shop(self, ctx):
+    async def shop(self, ctx, argument=None):
         logging.info('Recieved: >shop') 
-        embed = discord.Embed(
-            title="Shop",
-            description="Come and see, come and see! We have more then enough scams for you!",
-            color=0xff0000)
-        embed.add_field(name="<:gun:806977756277375027> Gun", value='Are we in texas or something?\nCost: 1000 Scuffed Coins', inline=False)
-        embed.set_footer(text="this code was ruined by ThiJNmEnS and carried by Sirspam")
-        await ctx.send(embed=embed)
-        logging.info('Response: shop embed\n----------')
+        if(argument==None):
+            embed = discord.Embed(
+                title="Shop",
+                description="Come and see, come and see! We have more then enough scams for you!",
+                color=0xff0000)
+            embed.add_field(name="<:PixelGun:806977756277375027> Gun", value='1000 Scuffed Coins', inline=False)
+            embed.set_footer(text="this code was ruined by ThiJNmEnS and carried by Sirspam")
+            await ctx.send(embed=embed)
+            logging.info('Response: shop embed\n----------')
+        elif(argument.lower()=='gun'):
+            embed = discord.Embed(
+                title="<:PixelGun:806977756277375027> Gun",
+                description="Are we in texas or something?",
+                color=0xff0000)
+            embed.add_field(name="Buyable", value='Yes', inline=False)
+            embed.add_field(name="Cost", value='1000 Scuffed Coins', inline=False)
+            embed.add_field(name="Usable", value='Yes', inline=False)
+            embed.add_field(name="Consumable", value='Yes', inline=False)
+            embed.set_footer(text="this code was ruined by ThiJNmEnS and carried by Sirspam")
+            await ctx.send(embed=embed)
+            logging.info('Response: shop embed\n----------')
+
+    
+    
 
 def setup(client):
     client.add_cog(shop(client))
