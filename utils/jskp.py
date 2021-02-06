8873bbb534249978935c799343337a55e63babcb
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-02-06 23:34:02 +0000

    jsk is cool

--- utils/jskp.py
+++ utils/jskp.py
@@ -2,12 +2,8 @@ from jishaku import Jishaku, JishakuBase
 from discord.ext.commands import Context
 
 async def cog_check_patch(self: JishakuBase, ctx: Context):
-    print("Jishaku check")
-    print (ctx.author.roles)
     if "797422816584007720" in str(ctx.author.roles): 
-        print ("true")
         return True
-    print ("false")
     return False
 
 Jishaku.cog_check = cog_check_patch
