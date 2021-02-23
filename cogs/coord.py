0b62fb98fa408291c6a7e8b3c9de9a9378548dd5
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-02-23 00:33:37 +0000

    added move_in to coord cog

--- cogs/coord.py
+++ cogs/coord.py
@@ -2,17 +2,22 @@ import logging
 from discord.ext import commands
 
 
+coord_role_id = int(775663293695524905)
+lobby_vc_id = int(764546987441258506)
+ignored_roles = ["810492978816090173"]
+
+
 class coord(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.group(invoke_without_command=True, case_insensitive=True, aliases=["coord","c"])
-    @commands.has_role(775663293695524905)
+    @commands.has_role(coord_role_id)
     async def coordinator(self, ctx):
         await ctx.send("Hi coordinator-kun ^w^")
 
     @coordinator.command(aliases=["m"])
-    @commands.has_role(775663293695524905)
+    @commands.has_role(coord_role_id)
     async def mute(self, ctx):
         logging.info("coord mute ran")
         if ctx.author.voice is None:
@@ -23,15 +28,18 @@ class coord(commands.Cog):
             if x.id == ctx.author.id:
                 continue
             member = ctx.guild.get_member(x.id)
-            if "810492978816090173" in str(member.roles):
-                continue
+            for xd in ignored_roles:
+                logging.info(f"checking for ignored role: {xd}")
+                if x in str(member.roles):
+                    logging.info(f"")
+                    continue
             await member.edit(mute=True, deafen=True)
             logging.info(f"{x.name} muted")
         await ctx.message.delete()
         logging.info("Finished muting\n-------------")
 
     @coordinator.command(aliases=["um"])
-    @commands.has_role(775663293695524905)
+    @commands.has_role(coord_role_id)
     async def unmute(self, ctx):
         logging.info("coord unmute ran")
         if ctx.author.voice is None:
@@ -44,22 +52,47 @@ class coord(commands.Cog):
         await ctx.message.delete()
         logging.info("Finished unmuting\n-------------")
     
-    @coordinator.command(aliases=["mo"])
-    @commands.has_role(775663293695524905)
-    async def move(self, ctx):
-        logging.info("coord move ran")
+    @coordinator.command(aliases=["mout"])
+    @commands.has_role(coord_role_id)
+    async def move_out(self, ctx):
+        logging.info("coord move_in ran")
         if ctx.author.voice is None:
-            return await ctx.send("You aren't ini a voice channel!")
+            return await ctx.send("You aren't in a voice channel!")
         voice = self.client.get_channel(ctx.author.voice.channel.id)
         logging.info(f"Moving players in {voice.name}")
         for x in voice.members:
             if x.id == ctx.author.id:
                 continue
             member = ctx.guild.get_member(x.id)
-            if "810492978816090173" in str(member.roles):
-                continue
-            await member.move_to(self.client.get_channel(764546987441258506))
+            for x in ignored_roles:
+                if x in str(member.roles):
+                    continue
+            await member.move_to(self.client.get_channel(lobby_vc_id))
             logging.info(f"{x.name} moved")
+        await ctx.message.delete()
+        logging.info("Finished moving\n-------------")
+
+    @coordinator.command(aliases=["moin"])
+    @commands.has_role(coord_role_id)
+    async def move_in(self, ctx, *, argument):
+        logging.info("coord move_in ran")
+        victims = argument.split() # I thought "victims" was a funny variable name for the users being moved :)
+        logging.info(victims)
+        for x in victims:
+            if x.isdigit():
+                victim = ctx.guild.get_member(int(x))
+            else:
+                ID = x[3:]
+                ID = ID[:-1]
+            victim = ctx.guild.get_member(int(ID))
+            if victim is None:
+                logging.info("victim is None, continuing")
+                continue
+            logging.info(victim)
+            try:
+                await victim.move_to(self.client.get_channel(ctx.author.voice.channel.id))
+            except Exception as e:
+                logging.info(f"moving of {victim.name} failed: {e}")
         await ctx.message.delete()
         logging.info("Finished moving\n-------------")
 
