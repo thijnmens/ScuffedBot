ba58c75196132630475995daa1be3aae4b87b86c
thijnmens <thijnsmulders04@gmail.com>
2021-03-01 11:13:55 +0100

    funni database

--- cogs/database.py
+++ cogs/database.py
@@ -13,8 +13,8 @@ class database(commands.Cog):
     @commands.command(case_insensitive=True)
     @commands.has_permissions(administrator=True)
     async def db(self, ctx, argument0=None, argument1=None, argument2=None):
+        logging.info("recieved: >db add")
         if argument0 == 'add' and argument1 == 'inv':
-            logging.info("recieved: >db add")
             ref = dab.collection('users').document('collectionlist').get().get('array')
             amount = len(ref) - 1
             count = 0
@@ -24,6 +24,19 @@ class database(commands.Cog):
                     inv = dab.collection('users').document(str(ID)).get().get('inv')
                     inv.append(argument2)
                     dab.collection('users').document(str(ID)).update({'inv': inv})
+                except Exception as e:
+                    logging.error(e)
+                count = count + 1
+            logging.info("updated database\n----------")
+            await ctx.send('I\'ve updated the database senpai UwU')
+        elif argument0 =='add':
+            ref = dab.collection('users').document('collectionlist').get().get('array')
+            amount = len(ref) - 1
+            count = 0
+            while (count <= amount):
+                try:
+                    ID = ref[count]
+                    inv = dab.collection('users').document(str(ID)).set({f'{argument1}': argument2})
                 except Exception as e:
                     logging.error(e)
                 count = count + 1
