344cbadb8d3ec445afb1765f27b2ebae80f44b38
notmynamex <59706340+notmynamex@users.noreply.github.com>
2021-04-18 22:49:59 +0200

    make spams retarded nhentai embed better
    spams sucks

--- cogs/nhentai.py
+++ cogs/nhentai.py
@@ -10,9 +10,22 @@ nhentai = NHentai()
 
 
 async def sauce_embed(sauce):
-    desc = "Tags: "
+    print(sauce)
+    Tags = "Tags: "
     for x in getattr(sauce, "tags"):
-        desc = desc + x + ", "
+        Tags = Tags + x + ", "
+    Artist = "Artist: "
+    for y in getattr(sauce, "artists"):
+        Artist = Artist + y + ", "
+    Parodies = "Parodies: "
+    for z in getattr(sauce, "parodies"):
+        Parodies = Parodies + z + ", "
+    sec_title = "Alternative Title: "
+    for a in getattr(sauce, "secondary_title"):
+        sec_title = sec_title + a 
+    characters = "Characters: "
+    for b in getattr(sauce, "characters"):
+        characters = characters + b + ", "
     if "english" in getattr(sauce, "languages"):
         lang = "🇬🇧"
     elif "japanese" in getattr(sauce, "languages"):
@@ -24,8 +37,28 @@ async def sauce_embed(sauce):
     embed = discord.Embed(
         title=f"{lang} "+getattr(sauce,"title"),
         url="https://nhentai.net/g/"+getattr(sauce,"id")+"/",
-        description=desc,
-        colour=0xec2753,
+        description=sec_title,
+        colour=0x1f13ee,
+    )
+    embed.add_field(
+        name="Tags",
+        value=Tags,
+        inline=False
+    )
+    embed.add_field(
+        name="Artist",
+        value=Artist,
+        inline=False
+    )
+    embed.add_field(
+        name="Parodies",
+        value=Parodies,
+        inline=False
+    )
+    embed.add_field(
+        name="Characters",
+        value=characters,
+        inline=False
     )
     embed.set_footer(text=str(getattr(sauce,"total_pages"))+" total pages")
     embed.set_image(url=(getattr(sauce,"images"))[0])
