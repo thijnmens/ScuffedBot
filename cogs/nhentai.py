83ef0108abe4868dea4f713f41829ab47fc8bceb
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-05-10 19:37:42 +0100

    patched rare issue with nhentai cog

--- cogs/nhentai.py
+++ cogs/nhentai.py
@@ -12,18 +12,26 @@ async def sauce_embed(sauce):
     tags = str()
     for x in getattr(sauce, "tags"):
         tags = tags + x + ", "
+    if tags == str():
+        tags = "None"
     artist = str()
     for x in getattr(sauce, "artists"):
         artist = artist + x + ", "
+    if artist == str():
+        artist = "None"
     parodies = str()
     for x in getattr(sauce, "parodies"):
         parodies = parodies + x + ", "
+    if artist == str():
+        artist = "None"
     sec_title = "**Alternative Title:** "
     for x in getattr(sauce, "secondary_title"):
         sec_title = sec_title + x
     characters = str()
     for x in getattr(sauce, "characters"):
         characters = characters + x + ", "
+    if artist == str():
+        artist = "None"
     if "english" in getattr(sauce, "languages"):
         lang = "🇬🇧"
     elif "japanese" in getattr(sauce, "languages"):
